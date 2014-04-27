db02116d883e086214bc80ea07b3d7f03045df8e
Jonas Raedle <jonas.raedle@gmail.com>
2014-04-27 23:54:08 +0200

    added todos and exercise 1.16

---
+++ ex1.16.scm
@@ -0,0 +1,11 @@
+;;linear time constant space implementation of exponentiation
+
+(define (fast-expt b n)
+  (define (fast-expt-iter a b n)
+    (cond ((= n 0) a)
+	  ((even? n) (fast-expt-iter a (* b b) (/ n 2)))
+	  (else (fast-expt-iter (* a b) b (- n 1)))))
+  (fast-expt-iter 1 b n))
+
+(define (even? n)
+  (= (remainder n 2) 0))

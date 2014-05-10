084ad34c20df350b3b50446b158142256b5742bb
Jonas Raedle <jonas.raedle@gmail.com>
2014-05-10 20:02:25 +0200

    exercise 1.27

---
+++ ex1.27.scm
@@ -0,0 +1,17 @@
+;;tests wether a number is carmichael
+
+(define (carmichael? n)
+  (define (carmichael-helper n a)
+    (cond ((= n a) #t)
+	  ((= a (expmod a n n)) (carmichael-helper n (+ a 1)))
+	  (else #f)))
+  (carmichael-helper n 1))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	 (remainder (* base (expmod base (- exp 1) m))
+		    m))))

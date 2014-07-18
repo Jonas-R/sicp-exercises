e6ffa7b72ebf033f7327975caa435aacff41bbc1
Jonas Raedle <jonas.raedle@gmail.com>
2014-07-18 17:39:39 +0200

    exercise 2.11

--- ch2/ex2.07_2.13.scm
+++ ch2/ex2.07_2.13.scm
@@ -21,12 +21,49 @@
 ;    also depends on the actual bounds of the intervals. (same for division)
 
 (define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-	(p2 (* (lower-bound x) (upper-bound y)))
-	(p3 (* (upper-bound x) (lower-bound y)))
-	(p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4)
-		   (max p1 p2 p3 p4))))
+  (cond ((and (>= (upper-bound x) 0) (>= (upper-bound y) 0)
+	      (>= (lower-bound x) 0) (>= (lower-bound y) 0))
+	 (make-interval (* (lower-bound x) (lower-bound y)) (* (upper-bound x) (upper-bound y))))
+	
+	((and (>= (upper-bound x) 0) (>= (upper-bound y) 0)
+	      (<  (lower-bound x) 0) (>= (lower-bound y) 0))
+	 (make-interval (* (lower-bound x) (upper-bound y)) (* (upper-bound x) (upper-bound y))))
+	
+	((and (>= (upper-bound x) 0) (>= (upper-bound y) 0)
+	      (>= (lower-bound x) 0) (<  (lower-bound y) 0))
+	 (make-interval (* (upper-bound x) (lower-bound y)) (* (upper-bound x) (upper-bound y))))
+	
+	((and (< (upper-bound x) 0) (>= (upper-bound y) 0)
+	      (< (lower-bound x) 0) (>= (lower-bound y) 0))
+	 (make-interval (* (lower-bound x) (upper-bound y)) (* (upper-bound x) (lower-bound y))))
+	
+	((and (>= (upper-bound x) 0) (< (upper-bound y) 0)
+	      (>= (lower-bound x) 0) (< (lower-bound y) 0))
+	 (make-interval (* (upper-bound x) (lower-bound y)) (* (lower-bound x) (upper-bound y))))
+	
+	((and (>= (upper-bound x) 0) (>= (upper-bound y) 0)
+	      (<  (lower-bound x) 0) (<  (lower-bound y) 0))
+	 (let ((p1 (* (lower-bound x) (lower-bound y)))
+	       (p2 (* (lower-bound x) (upper-bound y)))
+	       (p3 (* (upper-bound x) (lower-bound y)))
+	       (p4 (* (upper-bound x) (upper-bound y))))
+	   (make-interval (min p1 p2 p3 p4)
+			  (max p1 p2 p3 p4))))
+	
+	((and (< (upper-bound x) 0) (>= (upper-bound y) 0)
+	      (< (lower-bound x) 0) (<  (lower-bound y) 0))
+	 (make-interval (* (lower-bound x) (upper-bound y)) (* (lower-bound x) (lower-bound y))))
+	
+	((and (>= (upper-bound x) 0) (< (upper-bound y) 0)
+	      (<  (lower-bound x) 0) (< (lower-bound y) 0))
+	 (make-interval (* (upper-bound x) (lower-bound y)) (* (lower-bound x) (lower-bound y))))
+	
+	((and (< (upper-bound x) 0) (< (upper-bound y) 0)
+	      (< (lower-bound x) 0) (< (lower-bound y) 0))
+	 (make-interval (* (upper-bound x) (upper-bound y)) (* (lower-bound x) (lower-bound y))))
+	
+	((#t) (error "not a valid interval!"))))
+	 
 
 (define (div-interval x y)
   (if (= (width y) 0)

59300ed386ef9f933e373225254f4ebc5968af8f
Jonas Raedle <jonas.raedle@gmail.com>
2014-10-10 23:38:50 +0200

    exercise 4.11

---
+++ ch4/ex4.11.scm
@@ -0,0 +1,61 @@
+;; frames represented by list of bindings
+
+(define (enclosing-environment env) (cdr env))
+
+(define (first-frame env) (car env))
+
+(define the-empty-environment '())
+
+(define (make-frame variables values)
+  (map list variables values))
+
+(define (frame-variables frame) (map car frame))
+(define (frame-values frame) (map cdr frame))
+
+(define (add-binding-to-frame! var val frame)
+  (append! frame (list (list var val))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan bindings)
+      (cond ((null? bindings)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (caar bindings))
+             (cadar bindings))
+            (else (scan (cdr bindings)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan frame))))
+  (env-loop env))
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan bindings)
+      (cond ((null? bindings)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (caar bindings))
+             (set-car! bindings (list var val)))
+            (else (scan (cdr bindings)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+          (scan frame))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan bindings)
+      (cond ((null? bindings)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (caar bindings))
+             (set-car! bindings (list var val)))
+            (else (scan (cdr bindings)))))
+    (scan frame)))
